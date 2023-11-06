8f42f3f4b1de622a3166a64a11fe117b2f17e2ff
Wilmer Arambula <terabytesoftw@gmail.com>
2023-11-06 15:01:51 -0300

    Rework code `AjaxValidator::class`.

--- src/Validator/AjaxValidator.php
+++ src/Validator/AjaxValidator.php
@@ -4,18 +4,25 @@ declare(strict_types=1);
 
 namespace Yii\CoreLibrary\Validator;
 
-use Yii;
 use yii\base\ExitException;
 use yii\base\Model;
+use yii\web\Application;
 use yii\web\Request;
 use yii\web\Response;
 use yii\widgets\ActiveForm;
 
 /**
- * Ajax validation in controllers.
+ * Ajax validation services.
  */
-trait AjaxValidator
+final class AjaxValidator
 {
+    public function __construct(
+        private readonly Application $app,
+        private readonly Request $request,
+        private readonly Response $response
+    ) {
+    }
+
     /**
      * Perform Ajax validation for a given model.
      *
@@ -23,19 +30,14 @@ trait AjaxValidator
      *
      * @throws ExitException
      */
-    protected function performAjaxValidation(Model $model): void
+    public function validate(Model $model): void
     {
-        if (
-            $this->request instanceof Request &&
-            $this->response instanceof Response &&
-            $this->request->getIsAjax() &&
-            $model->load($this->request->post())
-        ) {
+        if ($this->request->getIsAjax() && $model->load($this->request->post())) {
             $this->response->format = Response::FORMAT_JSON;
             $this->response->data = ActiveForm::validate($model);
             $this->response->send();
 
-            Yii::$app->end();
+            $this->app->end();
         }
     }
 }
--- tests/Support/Controller/StubController.php
+++ tests/Support/Controller/StubController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Yii\CoreLibrary\Tests\Support\Controller;
 
+use yii\base\Module;
 use Yii\CoreLibrary\Validator\AjaxValidator;
 use yii\web\Controller;
 
@@ -12,10 +13,17 @@ use yii\web\Controller;
  */
 final class StubController extends Controller
 {
-    use AjaxValidator;
+    public function __construct(
+        $id,
+        Module $module,
+        private readonly AjaxValidator $ajaxValidator,
+        array $config = [],
+    ) {
+        parent::__construct($id, $module, $config);
+    }
 
     public function actionIndex($model): void
     {
-        $this->performAjaxValidation($model);
+        $this->ajaxValidator->validate($model);
     }
 }
